1aa1d3221e9e8d46a0b55a8e5e3827fe725346d2
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-17 22:07:20 +0200

    se muentra resultado y se dio formato css

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,14 +1,26 @@
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from "../constants"
 
 const Resultado = () => {
 
-    const {resultado} = useCotizador()
+    const {resultado, datos} = useCotizador()
+    const {marca, year, plan} = datos
+
+    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+    console.log(nombreMarca)
 
     if(resultado === 0) return null
 
     return (
-        <div>
-            {resultado}
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow-2xl">
+            <h2 className="text-gray-600 font-black text-3xl">
+                resumen
+            </h2>
+
+            <p className="my-2">
+                <span className="font-bold">Marca: </span>
+                {nombreMarca.nombre}
+            </p>
         </div>
     )
 }
